dfc0cccf2acb0f0281de112f19ff4c7ed5718814
Kensuke Nagae <kyanny@gmail.com>
2015-02-04 12:58:14 +0900

    Add

--- app/app.js
+++ app/app.js
@@ -6,7 +6,15 @@ i18n.init({
   load: 'current',
   lngWhitelist: ['en', 'es'],
   fallbackLng: 'en',
+  ns: {
+    namespaces: ['translation', 'qs', 'sa'],
+    defaultNS: 'sa'
+  },
+  fallbackToDefaultNS: true,
+  fallbackNS: 'translation',
+  resGetPath: '/locales/__lng__/__ns__.json',
   debug: true
 }, function(){
+  i18n.setDefaultNamespace('sa');
   greeting('greeting');
 });
